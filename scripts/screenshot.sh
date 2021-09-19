311585d64dac9654ba9563321c230224b8be480b
= <c.ringstrom@gmail.com>
2021-09-19 20:34:44 +0200

    Added better support for screenshots on multiple displays

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -6,10 +6,21 @@
 
 # Requires slurp, grim, mako and Sway
 
-# First function, requires time as input
+DISPLAY_1_WIDTH=1920
+#DISPLAY_2_WIDTH=1920
+
+
+# countdown function, requires time as input
 countdown () {
-	notify-send "Screenshot" "Screenshot in $1" -u low -t 1100 -i ~/.config/mako/monitor-screenshot.png
+	# Notifications depending on which display the screenshot was taken
+	if [ "$LEFT" = true ]; then
+		notify-send "Screenshot left" "Screenshot in $1" -u low -t 1100 -i ~/.config/mako/monitor-screenshot.png
+	fi
+	if [ "$RIGHT" = true ]; then
+		notify-send "Screenshot right" "Screenshot in $1" -u low -t 1100 -i ~/.config/mako/monitor-screenshot.png
+	fi
 }
+
 
 DELAY="$1"		# Takes first argument as delay in seconds
 if [[ ! $DELAY =~ ^[1-9][0-9]*$ ]]; then
@@ -18,7 +29,26 @@ if [[ ! $DELAY =~ ^[1-9][0-9]*$ ]]; then
 fi
 
 DIMENSIONS="$(slurp)"		# Records region
+# Multiple display support
+X_POS=$(echo "$DIMENSIONS" | awk -F '[,x ]' '{print $1}')
+#Y_POS=$(echo "$DIMENSIONS" | awk -F '[,x ]' '{print $2}')
+WIDTH=$(echo "$DIMENSIONS" | awk -F '[,x ]' '{print $3}')
+#HEIGHT=$(echo "$DIMENSIONS" | awk -F '[,x ]' '{print $4}')
 
+# Gets set to depending on which display the notification should appear on
+LEFT=false
+RIGHT=false
+
+if (( X_POS < DISPLAY_1_WIDTH ))
+then
+	LEFT=true
+fi
+if (( X_POS >= DISPLAY_1_WIDTH || X_POS + WIDTH >= DISPLAY_1_WIDTH ))
+then
+	RIGHT=true
+fi
+echo "$LEFT"
+echo "$RIGHT"
 # Countdown
 for (( i=DELAY; i>0; i--))
 do
@@ -31,5 +61,12 @@ sleep 0.1
 grim -g "$DIMENSIONS" -| wl-copy
 # Notifies user
 sleep 0.1
-notify-send "Screenshot" "Screenshot taken" -u low -i ~/.config/mako/monitor-screenshot.png
+
+# Notifications depending on which display the screenshot was taken
+if [ "$LEFT" = true ]; then
+	notify-send "Screenshot left" "Screenshot taken" -u low -i ~/.config/mako/monitor-screenshot.png
+fi
+if [ "$RIGHT" = true ]; then
+	notify-send "Screenshot right" "Screenshot taken" -u low -i ~/.config/mako/monitor-screenshot.png
+fi
 exit 0
